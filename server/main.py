c93f503cfea1635d6c7a0a16c2e28b87a0aad923
Itamar Shalev <itamar1111111@gmail.com>
2024-07-19 02:47:40 +0300

    server: main: Upload api to client side.
    Signed-off-by: Itamar Shalev <itamar1111111@gmail.com>

--- server/main.py
+++ server/main.py
@@ -23,5 +23,44 @@ async def upload_file(
     print(f"pdf reader text: {text}")
     return {"pdf_reader_text": text, "content": str(content), "description": position_description}
 
+
+@app.post("/api/get_stages_by_candidate/")
+async def get_stages_by_candidate(
+    candidate_phone_number: str = Form(...)
+):
+    return {[]}
+
+@app.post("/api/get_candidates_by_position/")
+async def get_candidates_by_position(
+    linkdin_url: str = Form(...),
+    user_name: str = Form(...)
+):
+    return {[]}
+
+@app.post("/api/add_linkdin_company/")
+async def add_company_linkdin(
+    company_linkdin_url: str = Form(...),
+    user_name: str = Form(...)
+):
+    return {[]}
+
+@app.post("/api/create_candidate/")
+async def create_candidate(
+    pdf_file: UploadFile = File(...),
+    positions_selected: str = Form(...)
+):
+
+    return {[]}
+
+@app.post("/api/get_positions_by_user_name/")
+async def get_positions_by_user_name(
+        user_name: str = Form(...)
+):
+    return {[]}
+
+@app.get("/api/get_positions/")
+async def get_positions():
+    return {[]}
+
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
